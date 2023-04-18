283ffb5f9cab2c2fe8425830848f38fbd9c06776
isnendyan <isnendyankp@gmail.com>
2023-04-18 10:18:11 +0700

    Services Component: create function services at data.js for export to services.js

--- src/components/Tours.js
+++ src/components/Tours.js
@@ -3,9 +3,7 @@ const Tours = () => {
   return (
     <section className="section" id="tours">
       <Title title='featured' subTitle='tours' />
-      {/* <div className="section-title">
-        <h2>featured <span>tours</span></h2>
-      </div> */}
+      
 
       <div className="section-center featured-center">
         <article className="tour-card">
