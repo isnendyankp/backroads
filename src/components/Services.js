7f0b5bab567c3b377a053425e3eee324ac044822
isnendyan <isnendyankp@gmail.com>
2023-04-18 11:27:49 +0700

    Tours Component: create & move tours info to data.js

--- src/components/Services.js
+++ src/components/Services.js
@@ -12,7 +12,7 @@ const Services = () => {
           <article className="service" key={id}>
           <span className="service-icon"><i className={icon}></i></span>
           <div className="service-info">
-            <h4 className="service-title">{title}}</h4>
+            <h4 className="service-title">{title}</h4>
             <p className="service-text">{text}</p>
           </div>
           </article>
