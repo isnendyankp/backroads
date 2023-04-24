6cb8c6b83a0d790bfb41681139281018ebb76f19
isnendyan <isnendyankp@gmail.com>
2023-04-24 13:06:03 +0700

    Check & fix: fix positions navbar

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,21 +1,18 @@
-import { socialLinks } from '../data'
+import { pageLinks, socialLinks } from '../data'
 import PageLinks from './PageLinks'
 import SocialLink from './SocialLink'
-
 const Footer = () => {
   return (
-    <footer className="section footer">
+    <footer className='section footer'>
       <PageLinks parentClass='footer-links' itemClass='footer-link' />
-      
-      <ul className="footer-icons">
+      <ul className='footer-icons'>
         {socialLinks.map((link) => {
-          const {id, href, icon} = link
           return <SocialLink key={link.id} {...link} itemClass='footer-icon' />
         })}
       </ul>
-      <p className="copyright">
+      <p className='copyright'>
         copyright &copy; Backroads travel tours company
-        <span id="date">{new Date().getFullYear()}</span> all rights reserved
+        <span id='date'>{new Date().getFullYear()}</span>. all rights reserved
       </p>
     </footer>
   )
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,20 +1,20 @@
 import logo from '../images/logo.svg'
-import { socialLinks } from '../data'
+import { pageLinks, socialLinks } from '../data'
 import PageLinks from './PageLinks'
 import SocialLink from './SocialLink'
-
 const Navbar = () => {
   return (
-    <nav className="navbar">
-      <div className="nav-center">
-        <div className="nav-header">
-          <img src={logo} className="nav-logo" alt="backroads" />
-          <button type="button" className="nav-toggle" id="nav-toggle">
-            <i className="fas fa-bars"></i>
+    <nav className='navbar'>
+      <div className='nav-center'>
+        <div className='nav-header'>
+          <img src={logo} className='nav-logo' alt='backroads' />
+          <button type='button' className='nav-toggle' id='nav-toggle'>
+            <i className='fas fa-bars'></i>
           </button>
         </div>
+        {/* <!-- left this comment on purpose --> */}
 
-        <PageLinks parentClasss='nav-links' itemClass='nav-link' />
+        <PageLinks parentClass='nav-links' itemClass='nav-link' />
 
         <ul className='nav-icons'>
           {socialLinks.map((link) => {
@@ -25,5 +25,4 @@ const Navbar = () => {
     </nav>
   )
 }
-
 export default Navbar
